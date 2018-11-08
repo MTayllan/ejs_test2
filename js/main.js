bb2db2e37f64190b717f290a2e55718d956b1eb4
Messias Tayllan <messiastayllan@gmail.com>
2018-11-08 18:58:20 -0300

    Base HTML CSS JS completa

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav();
+    
+    // Starts Sidenav
+    var sidenav = M.Sidenav.init($('.sidenav'));
+
+    // Starts Tabs
+    var tabs = M.Tabs.init($('.tabs'), {swipeable: true});
+
+    // Start Collapsible
+    var collapsible = M.Collapsible.init($('.collapsible'), 
+        { 
+            onOpenStart: function(){
+                $("#expand-icon").html('expand_less');
+            },
+            onCloseStart: function(){
+                $("#expand-icon").html('expand_more');
+            }
+        });
+
+    // Configura cor do icone de enviar
+    $('#send-text').focusin(function (){
+        $('#send-button').css('color', '#4BB0B8')
+    });
+
+    $('#send-text').focusout(function (){
+        $('#send-button').css('color', '#666')
+    });
+
 });
